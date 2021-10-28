c802bc3c65c1fcaafe60ee9e35a65f790ce3caf5
Elizabeth Battenfield <71232235+BattenfE8799@users.noreply.github.com>
2021-10-27 21:23:06 -0400

    Dataman-Sprint2
    Added Memory Bank game

--- Dataman/Dataman.py
+++ Dataman/Dataman.py
@@ -1,10 +1,9 @@
-#M2HW1 Sprint 1-AnswerChecker
+#M2HW1 Sprint 2- Memory Bank
 #CTS-285-0001
 #Elizabeth Battenfield
 
-"""
-Main menu
-"""
+import time
+import sys
 
 def main():
     """
@@ -14,15 +13,15 @@ def main():
     
     while keepGoing == True:
         print("\n","-"*8,"Menu","-"*8)
-        print("\n1. Answer Checker \n2. to do")
-        print("3. Memory Bank\n4. Number Guesser\n5. Exit")
+        print("\n1. Answer Checker \n2. Memory Bank")
+        print("3. Number Guesser\n5. Exit")
         
         choice = int(input("What do you want to do?: "))
         
         if choice == 1:
             answerChecker()
         elif choice == 2:
-            return
+            memoryBank()
         elif choice == 3:
             return
         elif choice == 4:
@@ -86,7 +85,101 @@ def calculate(x,t,y):
         exit
     
         
+def memoryBank():
+    """
+    - Saves 10 math problems intered by user
+    - displays those problems for user to answer
+    - times the user on how long it takes for them to answer the questions
+    - displays their score and time
+    """
+    print("\n\n","-"*8,"Memory Bank","-"*8)
+    print("1. Enter 10 new problems")
+    print("2. Answer the problems")
+    print("3. exit")
+    choice = int(input())
+    if choice == 1:
+        memBankProblems()
+    elif choice == 2:
+        memBankGame()
+    elif choice == 3:
+        goodBye()
         
+def memBankProblems():
+    print("Enter 10 math problems for us to remember!")
+    with open('memBankFile.txt', mode='w') as file:
+        for i in range(3):
+            x = input("Enter your first number: ")
+            t = input("Enter + , - , / , or * or exit: ")
+            y = input("Enter your second number: ")
+            print(x+t+y, file=file)
+            
+    return
+
+def memBankGame():
+    print("\n\n","~"*8,"Memory Bank Game","~"*8)
+    print("Enter your answer for each problem shown!")
+    print("You have 2 tries for each problem!")
+    print("If you want to quit type 'exit'.")
+    print("At the end of the game, I will tell you your score!")
+    print("Good luck!")
+    start = time.time()
+    score = 0
+    outOf = 0
+    with open('memBankFile.txt', mode='r') as file:   
+        for line in file:
+            outOf += 1
+            if "+" in line:
+                question = line.split("+")
+                x = int(question[0])
+                y = int(question[1])
+                t = "+"
+                z = calculate(x,t,y)
+                print(z)
+            elif "-" in line:
+                question = line.split("-")
+                x = int(question[0])
+                y = int(question[1])
+                t = "-"
+                z = calculate(x,t,y)
+            elif "/" in line:
+                question = line.split("/")
+                x = int(question[0])
+                y = int(question[1])
+                t = "/"
+                z = calculate(x,t,y)
+            elif "*" in line:
+                question = line.split("*")
+                x = int(question[0])
+                y = int(question[1])
+                t = "*"
+                z = calculate(x,t,y)
+                
+            print("\n",x, t, y)
+            count = 0
+            while count != 2:
+                answer = input("\nEnter your answer: ")
+                if answer == 'exit':
+                    goodBye()
+                else:
+                    answer = int(answer)
+                    if answer == z:
+                        print('Correct!')
+                        count = 2
+                        score += 1
+                        print(score, outOf)
+                    else:
+                        print("Wrong Answer. Try again.")
+                        count +=1
+                        print(score, outOf)
+    end = time.time()
+    timeLapsed = end - start
+    print("~"*4,"Score: ",score," out of ",outOf,"-"*4, round(timeLapsed), ' seconds')
+                
+def goodBye():
+    return
+    
+    
+    
     
         
         
